9dc3c7a83da8556d8001ea1230e2649ec29aed01
Geoffroy Weyer <webweyer@gmail.com>
2013-10-19 12:00:05 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Feedr::Application.config.secret_key_base = 'b820bb8dfbc22188b72fe2fb0c7949ee9e79eb7ed1d42c6f91d37b19a1919545c7c429f74f07cb620f319ce4f477392f8a98f49426b78cdbd5cec24d6bcf1180'
