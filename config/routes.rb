79cd04773e4bf038f67378bd0bdd84c906fd4443
Geoffroy Weyer <webweyer@gmail.com>
2013-10-19 13:47:19 +0200

    Fixes root url

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Feedr::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#index'
+  resources :homes, :only => :show
+  root 'homes#show'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
