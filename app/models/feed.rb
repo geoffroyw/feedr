25194a37b0c30d1b8b5b022fbb128fc7d80e6fd9
Geoffroy Weyer <webweyer@gmail.com>
2013-10-19 18:53:56 +0200

    Feed Model

---
+++ app/models/feed.rb
@@ -0,0 +1,8 @@
+class Feed < ActiveRecord::Base
+
+  flux_url_regexp = /\A((https?):\/\/|(www)\.)[a-z0-9-]+(\.[a-z0-9-]+)+([\/?].*)?\z/i
+  validates :url, presence: true
+  validates :url, uniqueness: {case_sensitive: false}
+  validates :url, format: {with: flux_url_regexp}
+  validates :name,presence: true
+end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,9 @@
 
 FactoryGirl.define do
   factory :user do
+    sequence :email do |n|
+      "mail#{n}@provider.tld"
+    end
+    password "P@ssword"
   end
 end
