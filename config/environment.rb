1327d73e466a92286e97f4591fed69ef044abab0
Geoffroy Weyer <webweyer@gmail.com>
2014-09-10 20:16:31 +0200

    removes faster require gem

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,4 @@
 # Load the Rails application.
-require File.dirname(__FILE__) + '/../lib/faster_require-0.9.4/lib/faster_require'
 require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
