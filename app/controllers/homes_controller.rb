b311f4884b684c28d38b84d1ef373b2389a4aec3
Geoffroy Weyer <webweyer@gmail.com>
2014-10-21 22:20:45 +0200

    shows last 15 items on home

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,8 @@
 class HomesController < ApplicationController
 
   def show
-    @feeds = current_user.feeds
+    #@feeds = current_user.feeds
+    @items = FeedItem.joins(:feed).where('feeds.id' => current_user.feeds.map{|f| f.id}).paginate(:page => params[:page], :per_page => 15)
   end
 
 end
