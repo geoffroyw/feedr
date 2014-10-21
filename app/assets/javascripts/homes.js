8fc492933c87506710c855fc944db13a216175a5
Geoffroy Weyer <webweyer@gmail.com>
2014-10-21 21:59:00 +0200

    add some interactivity with js

--- app/assets/javascripts/homes.js
+++ app/assets/javascripts/homes.js
@@ -1,3 +1,41 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+
+$(document).ready(function() {
+
+    var showAction = function(_this) {
+        _this.find('.actions').removeClass('hide');
+    }
+
+    var hideAction = function(_this) {
+        _this.find('.actions').addClass('hide')
+    }
+
+    $('.js-display-action').hover(function(){
+        showAction($(this))
+    }, function() {
+        hideAction($(this))
+    })
+
+    $('.js-show-content').on('click', function(event) {
+        event.preventDefault();
+
+        if ($(this).attr('data-action') == 'show') {
+            $(this).parents('.row').siblings('.content').removeClass('hide')
+
+            $(this).children('.glyphicon').removeClass('glyphicon-plus')
+            $(this).children('.glyphicon').addClass('glyphicon-minus')
+
+            $(this).attr('data-action', 'hide')
+        } else {
+            $(this).parents('.row').siblings('.content').addClass('hide')
+
+            $(this).children('.glyphicon').addClass('glyphicon-plus')
+            $(this).children('.glyphicon').removeClass('glyphicon-minus')
+
+            $(this).attr('data-action', 'show')
+
+        }
+
+    })
+
+
+})
